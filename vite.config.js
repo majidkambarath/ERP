5cbbb537dc374db36175eef6e8d5cf8bba97f026
ABDUL MAJID K <majidmaji3410@gmail.com>
2025-05-22 16:41:32 +0530

    update the url

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default ({ mode }) => {
   const proxy_url =
     process.env.VITE_DEV_REMOTE === 'remote'
       ? process.env.VITE_BACKEND_SERVER
-      : 'http://localhost:8888/';
+      : 'https://erp-node-ub9b.onrender.com/';
 
   const config = {
     plugins: [react()],
